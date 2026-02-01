7ed6866f95e4a0d337edf7922d38ee3ec178057e
Vishnu Thupakula <vishnuthupakulah@gmail.com>
2026-02-01 21:54:13 +0530

    Add files via upload

---
+++ 7_Loops.js
@@ -0,0 +1,58 @@
+// console.log("==========================> While <==========================");
+
+// function pftennum(){
+//     var num=1;
+//     while(num<=10){
+//         console.log(num);
+//         num++;
+//     }
+// }
+// pftennum();
+
+// console.log("==========================> do while <==========================");
+
+// function dowhile(){
+//     var num=10;
+//     do{
+//         console.log(num);
+//         num--;
+//     }while(num>=1);
+// }
+// dowhile();
+
+// console.log("==========================> for <==========================");
+
+// var result="";
+// for(var i=60;i<=100;i++){
+//     result+=i+" ";
+// }
+// console.log(result);
+
+// console.log("==========================> for in<==========================");
+// console.log("for...in loop is used to go through each key or index in an object or array.");
+
+// console.log("--------> for in (object) <--------")
+// var obj = {
+//     name: "Vishnu",
+//     age: 21,
+//     city: "Bangalore",
+//     id: 591
+// }
+
+// for(var key in obj){
+//     console.log(key," --> ",obj[key]);
+// }
+
+// console.log("--------> for in (array) <--------")
+// var arr=[10,20,30,40,50,60];
+// for(var inx in arr){
+//     console.log(inx,"-->",arr[inx]);
+// }
+
+
+// console.log("==========================> for with of<==========================");
+// var arr = [1, "Vishnu", { age: 21 }]
+// for (var val of arr) {
+//     console.log(val);
+// }
+
