3b44a21edcbfab609c281d956b279641b7af53e4
Vishnu Thupakula <vishnuthupakulah@gmail.com>
2026-02-01 17:11:25 +0530

    Add files via upload

---
+++ 6_Selection_Control_Statement.js
@@ -0,0 +1,80 @@
+// console.log("========================> if <========================");
+
+// function fun(num){
+//     if(num%2==0){
+//         console.log("Given number is even");
+//     }
+// }
+// fun(20);
+// fun(18);
+
+// console.log("========================> if and else <========================");
+
+// function fun(num){
+//     if(num%2==0){
+//         console.log("Given number is Even");
+//     }else{
+//         console.log("Given number is Odd");
+//     }
+// }
+// fun(20);
+// fun(35);
+
+// console.log("========================> elseif <========================");
+
+// function fun(num){
+//     if(num>=90){
+//         console.log("Grade A");
+//     }else if(num>=70){
+//         console.log("Grade B");
+//     }else if(num>=50){
+//         console.log("Grade C");
+//     }else if(num>=35){
+//         console.log("Grade D");
+//     }else{
+//         console.log("Fail");
+//     }
+// }
+
+// fun(96);
+// fun(86);
+// fun(68);
+// fun(40);
+// fun(28);
+
+// console.log("========================> Switch <========================");
+
+// function swch(num){
+//     switch(num){
+//         case 0:
+//             console.log("Zero");
+//             break;
+//         case 1:
+//             console.log("One");break;
+//         case 2:
+//             console.log("Two");break;
+//         case 3:
+//             console.log("Three");break;
+//         case 4:
+//             console.log("Four");break;
+//         case 5:
+//             console.log("Five");break;
+//         case 6:
+//             console.log("Six");break;
+//         case 7:
+//            console.log("Seven");break;
+//         case 8:
+//             console.log("Eight");break;
+//         case 9:
+//             console.log("Nine");break;
+//         default:
+//            console.log("It is not single digit number");
+        
+//     }
+// }
+// swch(0);
+// swch(2);
+// swch(7);
+// swch(9);
+// swch(10);
+
