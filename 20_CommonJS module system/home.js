4b2520e7d3e3b788e78fd635d8daf8fe0d88ef08
Vishnu Thupakula <vishnuthupakulah@gmail.com>
2026-02-16 15:32:03 +0530

    Add files via upload

---
+++ 20_CommonJS module system/home.js
@@ -0,0 +1,11 @@
+var homeName="Vishnu Thupakula";
+
+function display(){
+    console.log(homeName);
+    console.log("This is the homename");
+}
+
+module.exports={
+    homeName,
+    display
+};
---
+++ 20_CommonJS module system/index.js
@@ -0,0 +1,7 @@
+var home=require("./home.js");
+
+home.display();
+
+home.homeName;
+
+console.log(home.homeName);
